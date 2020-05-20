0f3c764ffad451f29b7551eff0678cc0ecd3b806
Brandon Istenes <bistenes@gmail.com>
2020-05-20 10:25:38 -0700

    Support date translation to French (#17)

--- src/util.ts
+++ src/util.ts
@@ -1,19 +1,19 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-dayjs.extend(utc);
+import moment from "moment";
 
 // We use UTC for the present TZ because the dates in the data
 // are all midnight Eastern Time, which lines up with UTC
 // but not with, e.g. Pacific Time.
 export function formatDate(strDate: string) {
-  const date = dayjs.utc(strDate);
-  const today = dayjs.utc(new Date());
+  const date = moment.utc(strDate);
+  const today = moment.utc(new Date());
   if (
     date.date() === today.date() &&
     date.month() === today.month() &&
     date.year() === today.year()
   ) {
-    return `Today`;
+    return moment()
+      .calendar()
+      .split(" ")[0]; // locale-specific 'Today'
   } else if (date.year() === today.year()) {
     return date.format("DD MMM");
   } else {
